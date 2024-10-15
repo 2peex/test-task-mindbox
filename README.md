0daaf49545b6c5cc2c67bc4a4fa1856f4cb9f84a
dima <dmitriikomarow@gmail.com>
2024-10-15 13:16:23 +0300

    changes readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # React + TypeScript + Vite + Framer Motion
+# Ссылка на деплой: [here](https://test-task-mindbox-5kmb2une3-espodnes-projects.vercel.app/).
+
+# Installation
+  cd todo-app
+  npm i
+  npm run start 
--- src/components/TodoTabs/TodoTabs.tsx
+++ src/components/TodoTabs/TodoTabs.tsx
@@ -17,7 +17,7 @@ const TabsButton: React.FC<TodoTabsProps & { title: string }> = ({
     <motion.button
       whileHover={{ scale: 1.1 }}
       whileTap={{ scale: 0.9 }}
-      transition={{ type: "spring", stiffness: 200 }}
+      transition={{ type: "keyframes", stiffness: 200 }}
       className={`${styles.tab} ${activeTab === tabValue ? styles.active : ""}`}
       onClick={() => {
         setActiveTab(tabValue);
